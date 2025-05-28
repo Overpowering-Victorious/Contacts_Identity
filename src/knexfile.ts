171437c8672b91f0500e5c11cb486a1a0f76e115
Abhibhav Om Tyagi <tyagi.abhibhav@gmail.com>
2025-05-28 14:54:11 +0530

    Fix1.02

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -4,17 +4,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const connection: string | Knex.PgConnectionConfig = process.env.DATABASE_URL
+  ? process.env.DATABASE_URL
+  : {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    };
+
 const config: Knex.Config = {
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: Number(process.env.DB_PORT),
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  },
+  connection,
   migrations: {
-    directory: path.resolve(__dirname, 'migrations'),
+    directory: path.join(__dirname, 'migrations'),
     extension: 'ts',
   },
 };
